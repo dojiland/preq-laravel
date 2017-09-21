9c89b321e8f02f797df2eec8d8a4d6271be1b449
Persevere Von <wu543065657@163.com>
2017-09-21 11:33:55 +0800

    修复类名错误

--- src/Facades/Preq.php
+++ src/Facades/Preq.php
@@ -2,7 +2,7 @@
 
 namespace Per3evere\Preq\Facades;
 
-class Facade extends \Illuminate\Support\Facades\Facade
+class Perq extends \Illuminate\Support\Facades\Facade
 {
     /**
      * Get the registered name of the component.
